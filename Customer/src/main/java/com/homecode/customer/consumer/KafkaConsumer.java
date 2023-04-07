e63e2e48852ce2461cc0bcc18988871f9510bdfd
cUserd <zabralalg@abv.bg>
2023-04-07 19:21:20 +0300

    Kafka consumer added

---
+++ Customer/src/main/java/com/homecode/customer/consumer/KafkaConsumer.java
@@ -0,0 +1,13 @@
+package com.homecode.customer.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "my-topic",groupId = "group_id")
+    public void consume(String message){
+        System.out.println("message " + message);
+    }
+}
